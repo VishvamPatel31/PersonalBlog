98f1afb1b03037680f6729ee9817ca733c7a3d20
VishvamPatel31 <96796901+VishvamPatel31@users.noreply.github.com>
2025-04-06 00:19:56 -0400

    data for blogs

---
+++ script/data.js
@@ -0,0 +1,22 @@
+const data =[
+{
+        image: "/images/blog4.png",
+        date: "April 5th, 2025",
+        title: "Blog four",
+        desc:"I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path."
+    },
+ {
+        image: "/images/blog4.png",
+        date: "April 5th, 2025",
+        title: "Blog five",
+        desc:"I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path."
+    },
+ {
+        image: "/images/blog6.png",
+        date: "April 5th, 2025",
+        title: "Blog six",
+        desc:"I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path."
+    },
+];
+
+export default data;
